26a1a604e59b404f0ea6e7ea471d80dab667f0c6
ShurutiThinkQ <122771652+ShurutiThinkQ@users.noreply.github.com>
2023-01-16 11:43:12 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,36 @@
+
+ class FirstProgram
+ {
+	 public static void main(String args[])
+	 {
+		byte a=100;
+		byte b=29;
+		
+		//byte sum= (byte)(a+b);
+		
+		//a= (byte)(a+b);
+
+       //  a=a+b;
+        a+=b;   // autocasting
+       
+
+   		
+		System.out.println(a);
+		
+		
+		
+		/*
+		float x=90.6f;
+		float y=78.6f;
+		
+		int sum1= (int)(x+y);
+		System.out.println(sum1);
+		
+		
+			
+		
+		*/
+	 }
+ }
+ 
+ // aritm :int
